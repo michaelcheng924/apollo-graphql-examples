f3198ff2d40384a48b932a83ea4dda43b5b81c14
Michael Cheng <cheng.c.mike@gmail.com>
2018-06-12 16:00:06 -0500

    display channels list

--- src/client/src/modules/channels/index.js
+++ src/client/src/modules/channels/index.js
@@ -5,10 +5,26 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { channelsQuery } from './graphql';
 
 class Channels extends Component {
+    renderChannel(channel) {
+        const { name } = channel.item;
+
+        return (
+            <Text style={styles.channel}>
+                {name}
+            </Text>
+        );
+    }
+    
     render() {
         return (
             <View>
                 <Text style={styles.header}>Channels</Text>
+                <FlatList
+                    data={this.props.channels}
+                    keyExtractor={({ id }) => id}
+                    renderItem={this.renderChannel}
+                    style={styles.channels}
+                />
             </View>
         );
     }
@@ -17,6 +33,12 @@ class Channels extends Component {
 const styles = StyleSheet.create({
     header: {
         fontSize: 20
+    },
+    channels: {
+        marginTop: 20
+    },
+    channel: {
+        color: 'green'
     }
 });
 
