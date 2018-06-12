d3ffe9f9d09e82bc173757ff6283e4d4b41e6017
Michael Cheng <cheng.c.mike@gmail.com>
2018-06-12 13:18:29 -0500

    heroku setup

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,2 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-
-import schema from './src/graphql/schema';
-
-const PORT = process.env.PORT || 5000;
-
-const app = express();
-
-app.use('/graphql', bodyParser.json(), graphqlExpress({
-    schema
-}));
-
-app.use('/graphiql', graphiqlExpress({
-    endpointURL: '/graphql'
-}));
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+require('babel-register');
+require('./server');
---
+++ src/server/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+
+import schema from './src/graphql/schema';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema
+}));
+
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+}));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
